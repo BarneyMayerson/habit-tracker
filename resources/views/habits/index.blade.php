01e19b6a69de32eda0ec25c203859c3ce3513ecb
Barney Mayerson <mr.barney.mayerson@gmail.com>
2022-12-27 15:52:50 +0300

    Install Tailwind CSS && app layout

---
+++ resources/views/habits/index.blade.php
@@ -0,0 +1,3 @@
+<x-app-layout>
+    <h2 class="text-blue-600 text-4xl font-bold">It works!</h2>
+</x-app-layout>
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./resources/**/*.blade.php", 
+    "./storage/framework/views/*.php",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
