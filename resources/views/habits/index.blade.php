b51ce305a7245be2841596faa5911b23c6b658dc
Barney Mayerson <mr.barney.mayerson@gmail.com>
2022-12-28 14:58:55 +0300

    Progress bar (percentage) wip

--- resources/views/habits/index.blade.php
+++ resources/views/habits/index.blade.php
@@ -15,7 +15,11 @@
             </div>
             <div class="flex basis-1/4 justify-center p-2.5">
               <button class="bg-primary-600 text-white font-semibold text-2xl rounded-lg px-4 py-2">+1</button>
-
+            </div>
+            <div x-data="{ percent: 30 }" class="flex basis-1/4 justify-center">
+              <div class="relative inline-flex items-center justify-center">
+                <div class="flex absolute inset-0 items-center justify-center text-sm font-semibold" x-text="`${percent}%`"></div>
+              </div>
             </div>
           </div>
         </div>
