ccc9361874b6e925e4152bab578664e1d1d67f12
Barney Mayerson <mr.barney.mayerson@gmail.com>
2022-12-28 16:14:46 +0300

    ExecutionButton (vue  + test) [see to vm VI]

---
+++ resources/js/components/ExecuteButton.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="flex basis-1/4 justify-center p-2.5">
+    <button
+      id="execute"
+      class="bg-primary-600 text-white font-semibold text-2xl rounded-lg px-4 py-2"
+    >
+      +1
+    </button>
+  </div>
+</template>
+
+<script setup>
+import { exec } from "child_process";
+import { ref } from "vue";
+
+const executions_count = ref(0);
+
+const newExecution = () => {
+  executions_count.value++;
+};
+</script>
