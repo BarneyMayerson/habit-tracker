43150e397f0751ec7e05f5f0606ac8ab9f9a58d3
Barney Mayerson <mr.barney.mayerson@gmail.com>
2023-02-20 16:53:30 +0300

    Backend test rework (Index)

--- tests/Feature/Habit/IndexTest.php
+++ tests/Feature/Habit/IndexTest.php
@@ -2,9 +2,11 @@
 
 namespace Tests\Feature\Habit;
 
+use App\Http\Resources\HabitResource;
 use App\Models\Habit;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Request;
 use Tests\TestCase;
 
 class IndexTest extends TestCase
@@ -14,12 +16,23 @@ class IndexTest extends TestCase
     /** @test */
     function it_shows_all_habits()
     {
-        $habits = Habit::factory(3)->create();
-
-
         $response = $this->withoutExceptionHandling()->get('/habits');
 
         $response->assertStatus(200);
-        $response->assertViewHas('habits', $habits);
+    }
+
+    /** @test */
+    function habits_list_is_fetched()
+    {
+        Habit::factory(3)->create();
+        $habitResource = HabitResource::collection(Habit::withCount('executions')->get());
+        $request = Request::create('/api/habits', 'get');
+        $expectedResource = $habitResource->response($request)->getData(true);
+
+        $response = $this->getJson('/api/habits');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson($expectedResource);
     }
 }
